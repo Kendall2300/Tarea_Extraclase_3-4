1e91030815b58e1c35539c21ce0609ac9078fdd2
Daniel Ureña <67337592+dlurena24@users.noreply.github.com>
2021-11-09 14:21:11 -0600

    Post

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
-const mongoose = require('mongoose')
+const express=require('express')
+const bodyParser=require('body-parser')
+const cors=require('cors')
+const app=express()
+const apiPort=3000
 
-mongoose
-    .connect('mongodb://127.0.0.1:27017/cinema', { useNewUrlParser: true })
-    .catch(e => {
-        console.error('Connection error', e.message)
-    })
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors())
+app.use(bodyParser.json())
 
-const db = mongoose.connection
+app.get('/',(req,res)=>{
+    res.send('Hello World!!')
+})
 
-module.exports = db
+app.listen(apiPort,()=>console.log(`Server running on port ${apiPort}`))
