0fe6f839339622d0bf49131f1f768cbf459ae5e8
Kendall Martinez <kendallmartinezcarvajal4@gmail.com>
2021-11-10 10:37:35 -0600

    Fix_front end

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import { NavBar } from '../components'
 import { MoviesList, MoviesInsert, MoviesUpdate } from '../pages'
@@ -10,15 +10,15 @@ function App() {
     return (
         <Router>
             <NavBar />
-            <Routes>
-                <Route path="/movies/list" exact component={<MoviesList/>} />
-                <Route path="/movies/create" exact component={<MoviesInsert/>} />
+            <Switch>
+                <Route path="/movies/list" exact component={MoviesList} />
+                <Route path="/movies/create" exact component={MoviesInsert} />
                 <Route
                     path="/movies/update/:id"
                     exact
                     component={MoviesUpdate}
                 />
-            </Routes>
+            </Switch>
         </Router>
     )
 }
